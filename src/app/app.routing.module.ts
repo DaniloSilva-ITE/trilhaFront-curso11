8b131bfb32b0a8ef1ce2ea9068fb47aecbe088b6
Danilo Silva <danilo.silva@itenabler.com.br>
2021-05-13 11:46:34 -0300

    Reparos e Inicio Projeto

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,48 @@
+
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+
+import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
+import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+},
+
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver
+    }
+  },
+  {
+    path: 'p/add',
+    component: PhotoFormComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+
+];
+
+@NgModule( {
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true } )
+  ],
+  exports: [
+    RouterModule
+  ]
+} )
+
+export class AppRoutingModule { }
